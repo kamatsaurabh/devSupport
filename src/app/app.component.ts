2306105e537d90fea0c4893543eaf96cc8f96407
kamatsaurabh <saurabh.kamat@intelegencia.com>
2024-05-17 19:55:39 +0530

    imported component and specify with issue listing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,15 @@ import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http'; 
 import config from 'devextreme/core/config';
 import { licenseKey } from './devextreme-license';
+import { Issue1Component } from './Issue1/issue1.component';
+import { Issue2Component } from './Issue2/issue2.component';
+import { Issue3Component } from './Issue3/issue3.component';
 config({
     licenseKey:licenseKey
 })
 @Component({
     standalone: true,
-    imports: [ RouterModule,HttpClientModule],
+    imports: [ RouterModule,HttpClientModule,Issue1Component,Issue2Component,Issue3Component],
     providers: [],
     selector: 'app-root',
     templateUrl: './app.component.html',
